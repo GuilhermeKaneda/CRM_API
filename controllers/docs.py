280990d5fd0978d0f12111811f11be063c8d3098
Gabriel Faria e Silva <gabrielfariasilva10@hotmail.com>
2025-07-22 13:03:13 -0300

    docs: functions docummented in docs.py

--- controllers/docs.py
+++ controllers/docs.py
@@ -6,6 +6,7 @@ from flask import request
 from flask_restful import Resource
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
+from werkzeug.datastructures import FileStorage
 
 load_dotenv()
 
@@ -15,7 +16,17 @@ GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID")
 
 class DocsResource(Resource):
 
-    def authenticate_google_drive(self):
+    def authenticate_google_drive(self) -> GoogleDrive:
+        """
+        Função que autentica com o Google Drive.
+
+        Busca um arquivo "my_credentials.json" que contém os dados para autenticação.
+        Caso esse arquivo não exista, busca pelo arquivo "client_secrets.json",
+        que contém a credencial original retirada do console do GCP.
+
+        ## Retorno 
+            Instância da classe GoogleDrive do módulo pydrive.
+        """
         gauth = GoogleAuth()
         gauth.LoadCredentialsFile(GOOGLE_CREDENTIALS_PATH)
 
@@ -33,7 +44,19 @@ class DocsResource(Resource):
         gauth.SaveCredentialsFile(GOOGLE_CREDENTIALS_PATH)
         return GoogleDrive(gauth)
 
-    def upload_to_google_drive(self, file):
+    def upload_to_google_drive(self, file: FileStorage) -> str | None:
+        """
+        Faz o upload de um arquivo para o Google Drive.
+
+        ## Parâmetros
+            **file** : *FileStorage*.
+            Arquivo passado na requisição do front.
+
+        ## Retorno
+            Uma *str* contendo a URL do arquivo no Google Drive, caso o processo seja bem-sucedido.
+
+            *None* caso não consiga realizar o upload.
+        """
         if not file or file.filename == "":
             return None
 
@@ -55,7 +78,6 @@ class DocsResource(Resource):
         return f"https://drive.google.com/file/d/{gfile['id']}/view?usp=sharing"
 
     def post(self) -> dict:
-        print("FILES:", request.files)
         if "file" not in request.files:
             return {"error": "Nenhum arquivo enviado."}, 400
 
